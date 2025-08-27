4c3104fd879203b2962d249f19f360ff53fe07d4
Thevindu Sathsara Gamage <thevidu2007627@gmail.com>
2025-08-27 07:57:19 +0530

    feat(ui): Added error page for meals and global not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Could not find requested resource</p>;
+    </main>
+  );
+}
