31941c717e62c80caa327871961fa03c3f428b8b
Thevindu Sathsara Gamage <thevidu2007627@gmail.com>
2025-08-27 15:01:57 +0530

    refactor: Replace database with original data and removed delay simulation codes from meals.js

--- src/lib/meals.js
+++ src/lib/meals.js
@@ -6,28 +6,19 @@ import fs from "node:fs";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  // Simulate a delay
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
   // Return all meals
   const stmt = db.prepare("SELECT * FROM meals");
   return stmt.all();
 }
 
 export async function getMealbySlug(slug) {
-  // Simulate a delay
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   // const stmt = db.prepare("SELECT 8 FROM meals WHERE slug = @slug");
   // return stmt.get({ slug });
-
   const stmt = db.prepare("SELECT * FROM meals WHERE slug = ?");
   return stmt.get(slug);
 }
 
 export async function saveMeal(meal) {
-  // Simulate a delay
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
   meal.slug = slugify(meal.title, { lower: true });
   meal.summary = xss(meal.summary);
   meal.instructions = xss(meal.instructions);
